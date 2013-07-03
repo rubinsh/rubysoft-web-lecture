15c3a1dd3564dcbf3b8b78871875935389f744d1
Shai Rubinshtein <rubinsh@gmail.com>
2013-07-03 19:10:49 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubysoftWebLecture::Application.config.secret_token = 'b6426f1652bdb86609d4a9fae47f9cc6e9c635a123b4340f89ac2bde7eaf6dc88c208716c02a26fcf313071834cf00243bd4c46fa21af4c6a9658e9f0c5b5de9'
